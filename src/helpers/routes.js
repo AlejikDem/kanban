5c66ca3815e2bdf6222e1f864ebcd57420f8bec9
Oleh Demchuk <demchukoleg88@gmail.com>
2018-06-13 17:03:13 +0300

    Add router

---
+++ src/helpers/routes.js
@@ -0,0 +1,12 @@
+import Project from '../scenes/Project/Project';
+
+const routes = [
+  {
+    title: 'Main',
+    url: '/',
+    exact: true,
+    component: Project,
+  }
+];
+
+export default routes;
---
+++ src/scenes/Project/Project.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import FieldContainer from '../../components/Field/FieldContainer';
+
+const Project = () => {
+  return (
+    <div>
+      <div></div>
+      <FieldContainer />
+    </div>
+  );
+};
+
+export default Project;
