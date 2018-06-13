99b43ad762c126fb1d8f8e86ff506062c4a58a45
Oleh Demchuk <demchukoleg88@gmail.com>
2018-06-13 17:14:23 +0300

    Make new component structure with scenes

--- src/scenes/Project/Project.jsx
+++ src/scenes/Project/Project.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { css } from 'emotion';
 
-import FieldContainer from '../../components/Field/FieldContainer';
+import FieldContainer from './components/Field/FieldContainer';
+
+const wrapper = css`
+  padding: 0 15px;
+  max-width: 1000px;
+  margin: 0 auto;
+`;
+
+const top = css`
+  display: flex;
+  align-items: stretch;
+`;
+
+const topSlot = css`
+  width: 50%;
+  height: 200px;
+`;
 
 const Project = () => {
   return (
-    <div>
-      <div></div>
+    <div className={wrapper}>
+      <div className={top}>
+        <div className={topSlot}></div>
+        <div className={topSlot}></div>
+      </div>
       <FieldContainer />
     </div>
   );
--- src/scenes/Project/components/Field/Field.jsx
+++ src/scenes/Project/components/Field/Field.jsx
@@ -3,11 +3,6 @@ import { css } from 'emotion';
 
 import ColumnContainer from '../Column/ColumnContainer';
 
-const Wrapper = css`
-  padding: 0 15px;
-  max-width: 1000px;
-`;
-
 const Columns = css`
   display: flex;
   justify-content: space-between;
@@ -29,16 +24,13 @@ const columnTitle = css`
 
 const Field = ({ columns }) => {
   return (
-    <div className={Wrapper}>
-      <div>Field</div>
-      <div className={Columns}>
-        {columns.map(item => (
-          <div className={columnWrapper} key={item.id}>
-            <div className={columnTitle}>{item.title}</div>
-            <ColumnContainer key={item.id} item={item} />
-          </div>
-        ))}
-      </div>
+    <div className={Columns}>
+      {columns.map(item => (
+        <div className={columnWrapper} key={item.id}>
+          <div className={columnTitle}>{item.title}</div>
+          <ColumnContainer key={item.id} item={item} />
+        </div>
+      ))}
     </div>
   );
 };
